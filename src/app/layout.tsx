10890d8d46a88e0476b33979ed4ab26ebb6ab514
mmarkch <mark110447@gmail.com>
2025-11-04 19:01:34 +0700

    ADD auth verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
 import type { ReactNode } from "react";
 import { ToastProvider } from "@/components/toast";
+import { AuthHandler } from "@/components/auth/authHandler";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      {/* Remove default browser body margin to avoid left gutter on some screens */}
-      <body className="min-h-screen m-0 bg-[#f5f6fa] font-dbheavent">
-        <ToastProvider>{children}</ToastProvider>
+      <body className="min-h-screen bg-[#f5f6fa] font-dbheavent">
+        <ToastProvider>
+          <AuthHandler>
+            {children}
+          </AuthHandler>
+        </ToastProvider>
       </body>
     </html>
   );
