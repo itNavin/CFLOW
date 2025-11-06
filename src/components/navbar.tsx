33946bb06023f444fbe56b692db90bb93c05d328
mmarkch <mark110447@gmail.com>
2025-11-07 01:40:07 +0700

    FIX navbar display

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -100,7 +100,7 @@ export default function Navbar() {
 
   return (
     <>
-      <div className="w-full flex items-center px-6 py-3 bg-white border-b shadow-sm font-dbheavent">
+      <div className="w-full flex items-center justify-between px-6 py-3 bg-white border-b shadow-sm font-dbheavent">
         {/* LEFT: logo + hamburger */}
         <div className="flex items-center gap-4 shrink-0 hidden lg:block">
           {courseId && (
